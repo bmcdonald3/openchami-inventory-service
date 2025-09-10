21a98272affc2c12f458fc9771c1521258831c4a
Ben McDonald <ben.mcdonald@hpe.com>
2025-09-10 11:22:13 -0700

    Temporary datastore methods

--- internal/datastore/datastore.go
+++ internal/datastore/datastore.go
@@ -1,28 +1,113 @@
 package datastore
 
-import "github.com/bmcdonald3/openchami-inventory-service/pkg/models"
+import (
+	"fmt"
+	"sync"
 
-type Datastore interface {
-	// --- Device Methods ---
-	CreateDevice(device *models.Device) error
-	GetDeviceByID(id string) (*models.Device, error)
-	GetDeviceByName(name string) (*models.Device, error)
-	ListDevices() ([]*models.Device, error)
-	UpdateDevice(device *models.Device) error
-	DeleteDevice(id string) error
+	"github.com/bmcdonald3/openchami-inventory-service/pkg/models"
+)
 
-	// --- Location Methods ---
-	CreateLocation(location *models.Location) error
-	GetLocationByID(id string) (*models.Location, error)
-	GetLocationByName(name string) (*models.Location, error)
-	ListLocations() ([]*models.Location, error)
-	UpdateLocation(location *models.Location) error
-	DeleteLocation(id string) error
-
-	// --- Event Methods ---
-	CreateEvent(event *models.Event) error
-	GetEventByID(id string) (*models.Event, error)
-	ListEvents() ([]*models.Event, error)
-	ListEventsByDeviceID(deviceID string) ([]*models.Event, error)
-	ListEventsByLocationID(locationID string) ([]*models.Event, error)
+// MemoryStore is an in-memory implementation of the Datastore interface.
+type MemoryStore struct {
+	mu        sync.RWMutex
+	devices   map[string]*models.Device
+	locations map[string]*models.Location
+	events    map[string]*models.Event
 }
+
+// NewMemoryStore creates and returns a new MemoryStore.
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		devices:   make(map[string]*models.Device),
+		locations: make(map[string]*models.Location),
+		events:    make(map[string]*models.Event),
+	}
+}
+
+// --- Device Methods ---
+
+func (s *MemoryStore) CreateDevice(device *models.Device) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, exists := s.devices[device.ID]; exists {
+		return fmt.Errorf("device with ID %s already exists", device.ID)
+	}
+	s.devices[device.ID] = device
+	return nil
+}
+
+func (s *MemoryStore) GetDeviceByID(id string) (*models.Device, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	device, exists := s.devices[id]
+	if !exists {
+		return nil, fmt.Errorf("device with ID %s not found", id)
+	}
+	return device, nil
+}
+
+func (s *MemoryStore) GetDeviceByName(name string) (*models.Device, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	for _, device := range s.devices {
+		if device.Name == name {
+			return device, nil
+		}
+	}
+	return nil, fmt.Errorf("device with name %s not found", name)
+}
+
+func (s *MemoryStore) ListDevices() ([]*models.Device, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	var allDevices []*models.Device
+	for _, device := range s.devices {
+		allDevices = append(allDevices, device)
+	}
+	return allDevices, nil
+}
+
+func (s *MemoryStore) UpdateDevice(device *models.Device) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, exists := s.devices[device.ID]; !exists {
+		return fmt.Errorf("device with ID %s not found", device.ID)
+	}
+	s.devices[device.ID] = device
+	return nil
+}
+
+func (s *MemoryStore) DeleteDevice(id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, exists := s.devices[id]; !exists {
+		return fmt.Errorf("device with ID %s not found", id)
+	}
+	delete(s.devices, id)
+	return nil
+}
+
+// --- Location Methods (To be implemented later) ---
+
+func (s *MemoryStore) CreateLocation(location *models.Location) error {
+	return fmt.Errorf("not implemented")
+}
+func (s *MemoryStore) GetLocationByID(id string) (*models.Location, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// ... and so on for other location methods
+
+// --- Event Methods (To be implemented later) ---
+
+func (s *MemoryStore) CreateEvent(event *models.Event) error {
+	return fmt.Errorf("not implemented")
+}
+
+// ... and so on for other event methods
